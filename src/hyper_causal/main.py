64fb2781b29c17e0cfe2862e5aaf0911e1136b63
Kevin Bönisch <keboen@web.de>
2024-08-24 18:00:50 +0200

    Started the WEB app_mode of HyperCausal and added multiple DecodingStrategies such as top_k, greedy_top_k, top_p and beam_search.

--- src/hyper_causal/main.py
+++ src/hyper_causal/main.py
@@ -2,20 +2,29 @@ import argparse
 from flask import Flask, g, render_template, request, jsonify, current_app
 import sys
 import os
+import traceback
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from causal_lm import CausalLM
 
 app = Flask(__name__, template_folder="./web/templates", static_folder="./web/static", static_url_path='/static')
 
 @app.route('/')
-def hello_world():
+def index():
     llm_instance = get_llm()
-    return render_template("html/index.html", 
-                           llm=llm_instance.model_name, 
-                           input=get_input(),
-                           max_new_tokens=get_max_tokens(),
-                           k=get_k(),
-                           tree_style=get_tree_style())
+    app_mode = get_app_mode()
+    print('App mode: ' + app_mode)
+    # Dependant on the app mode, we return different templates
+    if(app_mode == 'CLI'):
+        return render_template("html/hyper_causal.html", 
+                            llm=llm_instance.model_name, 
+                            input=get_input(),
+                            max_new_tokens=get_max_tokens(),
+                            k=get_k(),
+                            tree_style=get_tree_style())
+    elif(app_mode == 'WEB'):
+        return render_template("html/index.html")
+    else: 
+        return "Encountered unknown app mode - it must be either 'CLI' or 'WEB'."
 
 @app.route('/api/tokens/next', methods=['POST'])
 def get_next_tokens():
@@ -34,17 +43,20 @@ def get_next_tokens():
                 target_idx=None,
                 k = get_k() if overwrite_k == -1 else overwrite_k,
                 max_length=1)
+            # print(next)
 
             result['result'] = next # type: ignore
             result['status'] = 200
-            # print(result)
     except Exception as ex:
         print("Couldn't generate next token branches: ")
         print(ex)
+        print(traceback.format_exc())
     return jsonify(result)
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Flask App with Command Line Arguments')
+    # Possible app_modes are CLI or WEB
+    parser.add_argument('--app_mode', type=str, default='WEB', help='Default "CLI": Enter the prompt and parameters directly through the cli.\n "WEB": Enter the prompt and parameters through a web interface.')
     parser.add_argument('--host', type=str, default='127.0.0.1', help='Host IP address')
     parser.add_argument('--port', type=int, default=5678, help='Port number')
     parser.add_argument('--debug', type=bool, default=True, help='True/False whether the app should be started in debug mode (required True for hot reload)')
@@ -82,6 +94,12 @@ def get_llm():
         args = parse_arguments()
         current_app.config['llm'] = create_llm(args.llm)
     return current_app.config['llm']
+
+def get_app_mode():
+    if 'app_mode' not in current_app.config:
+        args = parse_arguments()
+        current_app.config['app_mode'] = args.app_mode
+    return current_app.config['app_mode']
 
 def get_tree_style():
     if 'tree_style' not in current_app.config:
