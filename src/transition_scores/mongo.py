2f33e88e4bd8d1ea4dd4564bb0348a59e3bf6119
Manuel Schaaf <manuel.schaaf@em.uni-frankfurt.de>
2025-01-21 19:33:45 +0100

    feat: Added mongo.py

---
+++ src/transition_scores/mongo.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass, field
+from typing import Literal
+
+from bson.dbref import DBRef
+
+from transition_scores.data import LogProbs
+from transition_scores.utils import DataClassMappingMixin
+
+# @dataclass
+# class SynthetizedObject:
+#     synth_chunks: str
+#     synth_fulltext: str
+
+
+# @dataclass
+# class SynthetizedText:
+#     agent: str
+
+
+@dataclass
+class BaseTransitionScore(DataClassMappingMixin):
+    log_probs: list[LogProbs]
+    type: str = field(init=False)
+
+    def __post_init__(self):
+        self.log_probs = [tuple(log_prob) for log_prob in self.log_probs]
+
+
+@dataclass
+class TextTransitionScore(BaseTransitionScore):
+    type: str = field(default="text", init=False)
+
+
+@dataclass
+class ChunkTransitionScore(BaseTransitionScore):
+    type: str = field(default="chunk", init=False)
+    start_idx: int
+    end_idx: int
+
+
+@dataclass
+class ChunkInContextTransitionScore(ChunkTransitionScore):
+    type: str = field(default="chunk-in-context", init=False)
+    context_start_idx: int
+
+
+@dataclass
+class TransitionScoreItem(DataClassMappingMixin):
+    ref: DBRef
+    model: str
+    provider: Literal["onnx", "transformers"]
+    transition_scores: BaseTransitionScore
+    variant: Literal["default", "8bit", "o1", "o2", "o3", "o4"] = "default"
--- src/transition_scores/utils.py
+++ src/transition_scores/utils.py
@@ -1,2 +1,15 @@
 class ModelIntializationError(Exception):
     pass
+
+
+class DataClassMappingMixin:
+    def keys(self):
+        return self.__dataclass_fields__.keys()
+
+    def __getitem__(self, key):
+        if hasattr(self, key):
+            value = getattr(self, key)
+            if isinstance(value, DataClassMappingMixin):
+                return dict(**value)
+            return value
+        raise KeyError(f"Key {key} not found in {type(self).__name__}")
