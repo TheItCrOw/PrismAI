009d62c56c3ae376f18c7ea1f95b78f026d2857d
Manuel Schaaf <manuel.schaaf@em.uni-frankfurt.de>
2025-01-23 17:00:00 +0100

    feat: Added helper classes for DB objects

--- src/transition_scores/data.py
+++ src/transition_scores/data.py
@@ -1,12 +1,18 @@
+from typing import Any, Literal, Self
+
+from bson.dbref import DBRef
+
+
 class TransitionScores(dict):
-    def __init__(
-        self,
+    @classmethod
+    def new(
+        cls,
         target_id: int,
         target_prob: float,
         top_k_ids: list[int],
         top_k_scores: list[float],
-    ):
-        super().__init__(
+    ) -> Self:
+        return cls(
             {
                 "target_id": target_id,
                 "target_prob": target_prob,
@@ -16,7 +22,122 @@ class TransitionScores(dict):
         )
 
     @classmethod
-    def new(cls, tuple_or_target_id: tuple | int, *args):
-        if isinstance(tuple_or_target_id, tuple):
-            return cls(*tuple_or_target_id)
-        return cls(tuple_or_target_id, *args)
+    def from_tuple(cls, tup: tuple[int, float, list[int], list[float]]) -> Self:
+        return cls.new(*tup)
+
+
+class ModelMetadata(dict):
+    @classmethod
+    def new(
+        cls,
+        name: str,
+        provider: Literal["onnx", "transformers"],
+        variant: Literal["default", "8bit", "o1", "o2", "o3", "o4"] = "default",
+        **metadata,
+    ) -> Self:
+        return cls(
+            {
+                "name": name,
+                "provider": provider,
+                "variant": variant,
+                "metadata": metadata,
+            }
+        )
+
+    @classmethod
+    def from_tuple(cls, tup: tuple) -> Self:
+        return cls.new(*tup)
+
+
+class PreProcessorMetadata(dict):
+    @classmethod
+    def new(
+        cls,
+        type: Literal["text", "chunk", "chunk-in-context"],
+        **metadata,
+    ) -> Self:
+        return cls(
+            {
+                "type": type,
+                "metadata": metadata,
+            }
+        )
+
+    @classmethod
+    def from_tuple(cls, tup: tuple) -> Self:
+        return cls.new(*tup)
+
+
+class ScoresDict(dict):
+    @classmethod
+    def new(
+        cls,
+        ref: DBRef | dict,
+        text_sha256: str,
+        transition_scores: list[TransitionScores],
+        **metadata,
+    ):
+        return cls(
+            {
+                "ref": ref,
+                "text_sha256": text_sha256,
+                "transition_scores": transition_scores,
+                "metadata": metadata,
+            }
+        )
+
+    @classmethod
+    def from_raw(
+        cls,
+        scores: dict[str, Any],
+        collection: str = "collected_items",
+        database: str | None = None,
+    ) -> Self:
+        return cls.new(
+            DBRef(collection, scores.pop("_id"), database=database),
+            scores.pop("text_sha256"),
+            scores.pop("transition_scores"),
+            **scores,
+        )
+
+
+class FeaturesDict(dict):
+    @classmethod
+    def new(
+        cls,
+        ref: DBRef | dict,
+        text_sha256: str,
+        model: ModelMetadata | dict,
+        pre_processor: PreProcessorMetadata | dict,
+        transition_scores: list[TransitionScores],
+        **metadata,
+    ) -> Self:
+        return cls(
+            {
+                "ref": ref,
+                "text_sha256": text_sha256,
+                "model": model,
+                "pre_processor": pre_processor,
+                "transition_scores": transition_scores,
+                "metadata": metadata,
+            }
+        )
+
+    @classmethod
+    def from_tuple(cls, tup: tuple) -> Self:
+        return cls.new(*tup)
+
+    @classmethod
+    def from_scores(
+        cls,
+        scores: "ScoresDict",
+        model_metadata: ModelMetadata,
+        pre_processor_metadata: PreProcessorMetadata,
+        **metadata,
+    ) -> Self:
+        return cls.new(
+            **scores,
+            model=model_metadata,
+            pre_processor=pre_processor_metadata,
+            **metadata,
+        )
