3c12ac2a8cde87f7e185148c293f8d604c9a5bab
Manuel Schaaf <manuel.schaaf@em.uni-frankfurt.de>
2025-01-23 17:01:37 +0100

    minor: Removed redundant code

--- src/transition_scores/mongo.py
+++ src/transition_scores/mongo.py
@@ -1,11 +1,3 @@
-from dataclasses import dataclass, field
-from typing import Literal
-
-from bson.dbref import DBRef
-
-from transition_scores.data import TransitionScores
-from transition_scores.utils import DataClassMappingMixin
-
 # @dataclass
 # class SynthetizedObject:
 #     synth_chunks: str
@@ -15,39 +7,3 @@ from transition_scores.utils import DataClassMappingMixin
 # @dataclass
 # class SynthetizedText:
 #     agent: str
-
-
-@dataclass
-class BaseTransitionScore(DataClassMappingMixin):
-    features: list[TransitionScores]
-    type: str = field(init=False)
-
-    def __post_init__(self):
-        self.features = [dict(scores) for scores in self.features]
-
-
-@dataclass
-class TextTransitionScore(BaseTransitionScore):
-    type: str = field(default="text", init=False)
-
-
-@dataclass
-class ChunkTransitionScore(BaseTransitionScore):
-    type: str = field(default="chunk", init=False)
-    start_idx: int
-    end_idx: int
-
-
-@dataclass
-class ChunkInContextTransitionScore(ChunkTransitionScore):
-    type: str = field(default="chunk-in-context", init=False)
-    context_start_idx: int
-
-
-@dataclass
-class TransitionScoreItem(DataClassMappingMixin):
-    ref: DBRef
-    model: str
-    provider: Literal["onnx", "transformers"]
-    transition_scores: BaseTransitionScore
-    variant: Literal["default", "8bit", "o1", "o2", "o3", "o4"] = "default"
