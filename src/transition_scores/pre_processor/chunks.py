7906c003c4b098926adf15a8432da05aca3d3cae
Manuel Schaaf <manuel.schaaf@em.uni-frankfurt.de>
2025-01-29 14:09:01 +0100

    refactor: Moved sort_by_column and group_by_column to utils.py

--- src/transition_scores/pre_processor/chunks.py
+++ src/transition_scores/pre_processor/chunks.py
@@ -1,108 +1,17 @@
 from hashlib import sha256
 from typing import Any
 
-import numpy as np
 from tqdm import tqdm
 from transformers import BatchEncoding
 
 from transition_scores.data import OutputProbabilities, PreProcessorMetadata
 from transition_scores.pre_processor.text import TextPreProcessor
-from transition_scores.utils import chunks_to_text, transpose_dict_of_lists
-
-
-def explode_column(row: dict[str, list], column: str = "encoding") -> dict[str, list]:
-    encoding = row.pop(column)
-    flattend = {key: [] for key in row.keys() | encoding.keys()}
-    for transposed in transpose_dict_of_lists(encoding, iter=True):
-        for key, [value] in row.items():
-            flattend[key].append(value)
-        for key, value in transposed.items():
-            flattend[key].append(value)
-    return flattend
-
-
-def group_by_column(
-    dataset: list[dict[str, Any]],
-    key_column: str = "_ref_id",
-    deduplicate: tuple[str, ...] | None = (
-        "_ref_id",
-        "ref_id",
-        "_orig_ref_id",
-        "orig_ref_id",
-    ),
-    aggregate: tuple[str, ...] | None = None,
-    into: str = "grouped",
-    pop_key_column: bool = False,
-) -> list[dict[str, Any]]:
-    """Group a dataset by a column and move other columns into a list.
-
-    Examples:
-        >>> dataset = [
-        ...     {"foo": 1, "bar": "baz", "values": [1,2,3]},
-        ...     {"foo": 1, "bar": "baz", "values": [4,5,6]},
-        ...     {"foo": 2, "bar": "qux", "values": [7,8,9]},
-        ... ]
-        >>> group_by_column(dataset, "foo", ("bar",), ("values",))
-        {1: {'bar': 'baz', 'values': [[1, 2, 3], [4, 5, 6]]}, 2: {'bar': 'qux', 'values': [[7, 8, 9]]}}
-
-    Args:
-        dataset (list[dict[str, Any]]): The dataset to group.
-        column (str): The column to group by.
-        deduplicate (tuple[str, ...]): Columns to deduplicate.
-            These columns will be moved into the parent dict and not aggregated into a list.
-            Duplicate values will be overwritten.
-        aggregate (tuple[str, ...]): Columns to map.
-            These columns will be moved into the parent dict and aggregated into a list.
-        into (str | None): The target column to move all remaining values into
-            that are not covered by `deduplicate` or `aggregate`.
-
-    Returns:
-        dict[str, list[dict[str, Any]]]: The grouped dataset.
-    """
-    grouped = dict()
-    for source in dataset:
-        key = source.pop(key_column) if pop_key_column else source[key_column]
-        target = grouped.setdefault(key, dict())
-
-        if deduplicate:
-            for k in deduplicate:
-                if k in source:
-                    target[k] = source.pop(k)
-
-        if aggregate:
-            for k in aggregate:
-                if k in source:
-                    target.setdefault(k, []).append(source.pop(k))
-
-        if source:
-            target.setdefault(into, []).append(source)
-    return list(grouped.values())
-
-
-def sort_by_column(
-    dataset: list[dict[str, Any | list]], sort_by: str, fields_to_sort: tuple[str, ...]
-) -> list[dict[str, Any]]:
-    """Sort the given `fields_to_sort` by the values provided in the given `sort_by` column.
-
-    Args:
-        dataset (list[dict[str, Any  |  list]]): The dataset to sort.
-        sort_by (str): The column to sort by containing lists of sortable values.
-        fields_to_sort (tuple[str, ...]): The fields to sort.
-            All fields must be lists of the same length as the column.
-
-    Returns:
-        list[dict[str, Any]]: The sorted dataset.
-    """
-    # lists & dicts are mutable, so we can just manipulate the values in-place
-    for row in dataset:
-        order = np.argsort(row[sort_by])
-        for field in fields_to_sort:
-            if len(row[field]) != len(row[sort_by]):
-                raise ValueError(
-                    f"Field '{field}' has a different length than the column '{sort_by}': {len(row[field])} != {len(row[sort_by])}"
-                )
-            row[field] = [row[field][i] for i in order]
-    return dataset
+from transition_scores.utils import (
+    chunks_to_text,
+    group_by_column,
+    sort_by_column,
+    transpose_dict_of_lists,
+)
 
 
 class RollingWindowChunkPreProcessor(TextPreProcessor):
