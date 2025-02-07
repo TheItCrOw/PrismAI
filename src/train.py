39386d0a9ba798ed12423ff34b35c71ad599c2fc
Manuel Schaaf <manuel.schaaf@em.uni-frankfurt.de>
2025-02-07 17:19:47 +0100

    minor: Updated train.py script

--- src/train.py
+++ src/train.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import traceback
 from itertools import product
@@ -14,58 +15,79 @@ from luminar.mongo import MongoDBAdapter
 
 load_dotenv("../env")
 seed_everything(42)
-# for bs, fs in product((32, 64, 128), (128, 256, 512)):
-for feature_size in (64, 128, 256):
-    feature_size = OneDimFeatures(feature_size)
-    for domain in (
-        # "arxiv_papers",
-        # "blog_authorship_corpus",
-        # "bundestag",
-        # "cnn_news",
-        # "euro_court_cases",
-        # "gutenberg",
-        # "house_of_commons",
-        # "spiegel_articles",
-        "student_essays",
-    ):
-        try:
-            db = MongoDBAdapter(
-                os.environ.get("MONGO_DB_CONNECTION"),
-                "prismai",
-                "collected_items",
-                "synthesized_texts",
-                "transition_scores",
-                domain=domain,
-                source_collection_limit=1500,
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "domains",
+        type=str,
+        nargs="+",
+        default=(
+            "blog_authorship_corpus",
+            "student_essays",
+            "cnn_news",
+            "bundestag",
+            "euro_court_cases",
+            "house_of_commons",
+        ),
+        choices=(
+            (
+                "blog_authorship_corpus",
+                "student_essays",
+                "cnn_news",
+                "bundestag",
+                "euro_court_cases",
+                "house_of_commons",
+                "spiegel_articles",
+                "arxiv_papers",
+                "gutenberg",
             )
-            seed_everything(42)
-            dm = DocumentClassificationDataModule(db, feature_size)
-            for lr, pdim in product((0.0005, 0.0001), (32, 64, 128)):
-                try:
-                    # for lr, wu, pr in product((1e-3, 1e-4, 1e-5), (0, 50, 100), (64, 128, 256)):
-                    seed_everything(42)
-                    model = DocumentClassficationModel(
-                        dm.feature_selection.effective_shape(),
-                        projection_dim=pdim,
-                        learning_rate=lr,
-                        warmup_steps=100,
-                    )
-                    trainer = Trainer(
-                        max_epochs=50,
-                        logger=pl_loggers.TensorBoardLogger(
-                            save_dir="logs/",
-                            name=domain,
-                        ),
-                        gradient_clip_val=0.5,
-                        deterministic=True,
-                        callbacks=[EarlyStopping(monitor="val_loss", mode="min")],
-                    )
-                    # tuner = Tuner(trainer)
-                    trainer.fit(model, dm)
-                    (metrics,) = trainer.validate(
-                        model, dataloaders=[dm.val_dataloader()]
-                    )
-                except Exception:
-                    traceback.print_exc()
-        except Exception:
-            traceback.print_exc()
+        ),
+    )
+    args = parser.parse_args()
+
+    for domain in args.domains:
+        for feature_size in (32, 64, 128, 256):
+            feature_size = OneDimFeatures(feature_size)
+            try:
+                db = MongoDBAdapter(
+                    os.environ.get("MONGO_DB_CONNECTION"),
+                    "prismai",
+                    "collected_items",
+                    "synthesized_texts",
+                    "log_likelihoods",
+                    domain=domain,
+                    source_collection_limit=1500,
+                )
+                seed_everything(42)
+                dm = DocumentClassificationDataModule(db, feature_size)
+                for lr, pdim, wus in product(
+                    (0.0005, 0.0001), (8, 16, 32, 64, 128, 256), (0, 100)
+                ):
+                    try:
+                        seed_everything(42)
+                        model = DocumentClassficationModel(
+                            dm.feature_selection.effective_shape(),
+                            projection_dim=pdim,
+                            learning_rate=lr,
+                            warmup_steps=wus,
+                        )
+                        trainer = Trainer(
+                            max_epochs=50,
+                            logger=pl_loggers.TensorBoardLogger(
+                                save_dir="logs/",
+                                name=domain,
+                            ),
+                            gradient_clip_val=0.5,
+                            deterministic=True,
+                            callbacks=[EarlyStopping(monitor="roc_auc", mode="max")],
+                        )
+                        # tuner = Tuner(trainer)
+                        trainer.fit(model, dm)
+                        (metrics,) = trainer.validate(
+                            model, dataloaders=[dm.val_dataloader()]
+                        )
+                    except Exception:
+                        traceback.print_exc()
+            except Exception:
+                traceback.print_exc()
